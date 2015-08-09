0df5a2988d0d50d409b308b46a126822b4bde197
GhozyLab <GhozyLab@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-09 02:00:10 +0000

    Release 1.1.25
    git-svn-id: https://plugins.svn.wordpress.org/image-slider-widget/trunk@1216125 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- inc/ewic-widget.php
+++ inc/ewic-widget.php
@@ -3,42 +3,55 @@
 class ewic_sc_widget extends WP_Widget {
 
     // Create Widget
-    function ewic_sc_widget() {
-        parent::WP_Widget(false, $name = 'Easy Slider Widget', array('description' => 'Use this widget to display your images slider.'));
+    function __construct() {
+		
+		$widget_ops = array('classname' => 'widget_ewic_sc_widget', 'description' => __( "Use this widget to display your images slider.") );
+        $control_ops = array( 'width' => 295 );
+
+		parent::__construct('ewic-widget', __('Image Slider Widget'), $widget_ops, $control_ops );
+		
     }
 
     // Widget Content
     function widget($args, $instance) { 
+	
         extract( $args );
-		if ( isset( $instance['ewic_shortcode'] ) ) {
-        	$ewic_shortcode = strip_tags($instance['ewic_shortcode']);
-			}
-        ?>
-            <div id="latest-box">
-                <span class="latest-text">
-                <?php
-				if ( isset( $ewic_shortcode ) ) {
-					echo do_shortcode( '[espro-slider id="'.$ewic_shortcode.'" iswidget="widget"]' );
-					}
-				?> 
-                </span> <!-- text -->
-            </div> <!-- box -->
-        <?php
+		
+		if ( isset ( $instance['ewic_shortcode'] ) ) {
+		
+        	$ewic_shortcode = $instance['ewic_shortcode'];
+			
+			$ewic_do_widget = do_shortcode( '[espro-slider id="'.$ewic_shortcode.'" iswidget="widget"]' );
+			
+		} else {
+			
+			$ewic_do_widget = '<p>Please select slider first.</p>';
+			
+		}
+		
+		
+		echo $before_widget;
+			
+		echo $ewic_do_widget;
+			
+		echo $after_widget;
+		
      }
 
     // Update and save the widget
     function update($new_instance, $old_instance) {
-    $instance = $old_instance;
-    $instance['ewic_shortcode'] = $new_instance['ewic_shortcode'];
-    return $new_instance;
+		
+    	$instance = $old_instance;
+	
+    	$instance['ewic_shortcode'] = $new_instance['ewic_shortcode'];
+	
+    	return $new_instance;
+	
     }
 
     // If widget content needs a form
-    function form($instance) {
-        //widgetform in backend
-		if ( isset( $ewic_shortcode ) ) {
-        	$ewic_shortcode = strip_tags($instance['ewic_shortcode']);
-			}
+    function form( $instance ) {
+	
         ?>
         <p><label for="<?php echo $this->get_field_id('ewic_shortcode'); ?>">Select your Slider name and hit save button.<br />
     <select id="<?php echo $this->get_field_id('ewic_shortcode'); ?>" name="<?php echo $this->get_field_name('ewic_shortcode'); ?>" >
@@ -62,7 +75,13 @@ endforeach;
         <?php       
     }
 }
-add_action('widgets_init', create_function('', 'return register_widget("ewic_sc_widget");'));
 
+function ewic_widget_init() {
+	
+	register_widget('ewic_sc_widget');
+	
+}
+
+add_action( 'widgets_init', 'ewic_widget_init' );
 
 ?>
--- readme.txt
+++ readme.txt
@@ -1,10 +1,10 @@
  === Image Slider ===
 Contributors: GhozyLab
 Donate link: http://ghozylab.com/donate
-Tags: image, slider, image slider, plugin slider, slider plugin, best slider, wordpress slider, images, free slider, wp slider, best slider plugin, meta slider, coin slider, facebook, google, wordpress, twitter, image slideshow, images slider, jquery slider, jquery slideshow, photo slider, picture slider, plugin, plugins, admin, page, post, posts, responsive slider, responsive slideshow, seo, slide, slider shortcode, slider widget, slides, slideshow, slideshow plugin, widget, widget slider, shortcode, widget slideshow, widgets, wordpress slideshow, carousel, carousel slider
+Tags: image, slider, image slider, plugin slider, slider plugin, best slider, gallery slider, wordpress slider, images, free slider, wp slider, best slider plugin, meta slider, coin slider, facebook, google, wordpress, twitter, image slideshow, images slider, jquery slider, jquery slideshow, photo slider, picture slider, plugin, plugins, admin, page, post, posts, responsive slider, responsive slideshow, seo, slide, slider shortcode, slider widget, slides, slideshow, slideshow plugin, widget, widget slider, shortcode, widget slideshow, widgets, wordpress slideshow, carousel, carousel slider
 Requires at least: 3.5
 Tested up to: 4.2.4
-Stable tag: 1.1.23
+Stable tag: 1.1.25
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -159,6 +159,11 @@ But seriously, I just want to drink coffee for free, so help a developer out. Yo
 
 == Changelog ==
 
+= 1.1.25 =
+* Fixed  : Widget issue for version 4.2.4 and ready for 4.3 ( Release on August 18 )
+* Fixed : Undefined variable ( PHP ) on shortcode
+* Fixed : Many other PHP and CSS clean and optimization
+
 = 1.1.23 =
 * Added  : Note inside media manager to let users know that they can easily insert multiple images at once
 * Fixed : Disable Metabox Toggle to avoid Slider Image Picker and Settings Page disappear
@@ -250,5 +255,5 @@ There are no complicated instructions for using Image Slider plugin because this
 
 == Upgrade Notice ==
 
-= 1.1.23 =
+= 1.1.25 =
 IMPORTANT! SECURITY BUGS FIX, PLEASE UPDATE NOW!
